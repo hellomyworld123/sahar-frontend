763172b92bc1c5763183e25aff4f0c91cb0a48d7
Votre Nom <votre.email@example.com>
2025-05-29 08:15:28 +0200

    feat: add API URL logging for debugging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+console.log('REACT_APP_API_URL =', process.env.REACT_APP_API_URL);
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
