8c6df612b48adb99ba584ae876fca10a247036e6
Votre Nom <votre.email@example.com>
2025-05-29 08:23:29 +0200

    fix: reorder imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-console.log('REACT_APP_API_URL =', process.env.REACT_APP_API_URL);
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+console.log('REACT_APP_API_URL =', process.env.REACT_APP_API_URL);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
